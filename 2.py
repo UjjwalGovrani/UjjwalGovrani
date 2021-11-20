3fac1a4a1ed7220afc101e1e13963771b265cfca
UjjwalGovrani <91419779+UjjwalGovrani@users.noreply.github.com>
2021-11-20 18:14:49 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,7 @@
+# Task2
+# Ujjwal Govrani
+rows = 5
+for i in range(rows, 0, -1):
+    for j in range(0, i + 1):
+        print(j, end=' ')
+    print(" ")
---
+++ 7.py
@@ -0,0 +1,10 @@
+# Task7
+# Ujjwal Govrani
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(1, rows + 1):
+        if j <= i:
+            print(i, end=' ')
+        else:
+            print(j, end=' ')
+    print()
